1d4211337822c4ea8a723dd2784b48ddbf232c5a
Morris Odioko <69381380+wealthychurch@users.noreply.github.com>
2020-08-31 18:24:09 -0500

    Update TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -33,6 +33,13 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
+    String someone = "World";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }  
+  
+    @Test
+  public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
